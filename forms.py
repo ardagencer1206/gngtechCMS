0cb11c9110e575fc6a7c91a94408ccf84ecf45c0
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2026-05-11 10:23:30 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired
 
 class HeroContentForm(FlaskForm):
@@ -20,4 +20,14 @@ class PatentForm(FlaskForm):
     title_en = StringField('Başlık (EN)', validators=[DataRequired()])
     desc_tr = TextAreaField('Açıklama (TR)', validators=[DataRequired()])
     desc_en = TextAreaField('Açıklama (EN)', validators=[DataRequired()])
+    submit = SubmitField('Kaydet')
+
+class JobPostingForm(FlaskForm):
+    title_tr = StringField('İş Başlığı (TR)', validators=[DataRequired()])
+    title_en = StringField('İş Başlığı (EN)', validators=[DataRequired()])
+    description_tr = TextAreaField('Görev Tanımı (TR)', validators=[DataRequired()])
+    description_en = TextAreaField('Görev Tanımı (EN)', validators=[DataRequired()])
+    requirements_tr = TextAreaField('Yetkinlikler (TR)', validators=[DataRequired()])
+    requirements_en = TextAreaField('Yetkinlikler (EN)', validators=[DataRequired()])
+    is_active = BooleanField('Aktif İlan', default=True)
     submit = SubmitField('Kaydet')
